c64597c5fb3b7c0e50797ad5bce82ef062e55d65
sseung0703 <lsh910703@gmail.com>
2025-06-03 19:30:25 +0900

    bug fix

--- vllm/model_executor/layers/quantization/awq_marlin.py
+++ vllm/model_executor/layers/quantization/awq_marlin.py
@@ -270,7 +270,7 @@ class AWQMarlinLinearMethod(LinearMethodBase):
         # Allocate marlin workspace
         layer.workspace = marlin_make_workspace_new(device)
 
-        if layer.qweight.dtype in [torch.float16, torch.bfloat16, torch.float32]:
+        if layer.qweight.shape[0] == layer.input_size_per_partition:
             # Repack weights from AWQ format to marlin format.
             marlin_qweight = ops.awq_marlin_repack(
                 layer.qweight,
